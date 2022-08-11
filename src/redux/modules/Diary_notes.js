5e2d0df2270d65ffc4cb86e298a4ba5dd065e04e
cordplace <msk7316@gmail.com>
2022-08-11 10:42:19 +0900

    다이어리 노트 메인에 있는 브랜치 가져옴

--- src/redux/modules/Diary_notes.js
+++ src/redux/modules/Diary_notes.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export const fetchUser = createAsyncThunk("users/fetchUser", async () => {
   return await axios
-    .get(`https://jjangudiary.herokuapp.com`)
+    .get(`http://localhost:3001/notes`)
     .then((res) => res.data)
     .catch((error) => error);
 });
@@ -11,7 +11,7 @@ export const addfetchUser = createAsyncThunk(
   "users/fetchUser",
   async (payload, thunkAPI) => {
     const resdata = await axios
-      .post(`https://jjangudiary.herokuapp.com`, {
+      .post(`http://localhost:3001/notes`, {
         id: payload.id,
         name: payload.name.userName,
         title: payload.title.userTitle,
@@ -29,7 +29,7 @@ export const patchfetchUser = createAsyncThunk(
     console.log(payload);
     const resdata = await axios
       // eslint-disable-next-line no-template-curly-in-string
-      .patch(`https://jjangudiary.herokuapp.com/${payload.id}`, {
+      .patch(`http://localhost:3001/notes/${payload.id}`, {
         id: payload.id,
         name: payload.name,
         contents: payload.contents,
